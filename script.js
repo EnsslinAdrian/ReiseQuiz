c4c39a3de6a3a974f287b9d0fda907a3ee54f943
EnsslinAdrian <ensslin.adrian@web.de>
2024-04-26 23:36:53 +0200

    new start 1

---
+++ script.js
@@ -0,0 +1,96 @@
+let questions = [
+  {
+    "question": "Welches Land ist bekannt für seine historischen Pyramiden?",
+    "answer_1": "Indien",
+    "answer_2": "Ägypten",
+    "answer_3": "Mexiko",
+    "answer_4": "China",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Was ist die Hauptstadt von Australien?",
+    "answer_1": "Sydney",
+    "answer_2": "Canberra",
+    "answer_3": "Melbourne",
+    "answer_4": "Perth",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welche Stadt wird oft als 'die Stadt der Liebe' bezeichnet?",
+    "answer_1": "Rom",
+    "answer_2": "Paris",
+    "answer_3": "Venedig",
+    "answer_4": "Madrid",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welche Insel ist bekannt für ihre Moai-Statuen?",
+    "answer_1": "Fidschi",
+    "answer_2": "Osterinsel",
+    "answer_3": "Bali",
+    "answer_4": "Sizilien",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welches Land ist berühmt für seine Fjorde?",
+    "answer_1": "Schweden",
+    "answer_2": "Norwegen",
+    "answer_3": "Neuseeland",
+    "answer_4": "Kanada",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welche Stadt ist bekannt für den Karnak-Tempel?",
+    "answer_1": "Kairo",
+    "answer_2": "Luxor",
+    "answer_3": "Alexandria",
+    "answer_4": "Aswan",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welches Land hat die meisten Inseln weltweit?",
+    "answer_1": "Griechenland",
+    "answer_2": "Schweden",
+    "answer_3": "Indonesien",
+    "answer_4": "Philippinen",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welche Stadt ist bekannt für den Sitz des Papstes?",
+    "answer_1": "Mailand",
+    "answer_2": "Vatikanstadt",
+    "answer_3": "Florenz",
+    "answer_4": "Neapel",
+    "right_answer": 2
+  },
+
+  {
+    "question": "Welcher Berg ist der höchste Gipfel der Welt?",
+    "answer_1": "K2",
+    "answer_2": "Mount Everest",
+    "answer_3": "Mount Kilimanjaro",
+    "answer_4": "Mont Blanc",
+    "right_answer": 2
+  },
+
+  {
+    "question": "In welchem Land befindet sich die Ruinenstadt Machu Picchu?",
+    "answer_1": "Brasilien",
+    "answer_2": "Peru",
+    "answer_3": "Chile",
+    "answer_4": "Kolumbien",
+    "right_answer": 2
+  },
+
+];
+
+
+
+
